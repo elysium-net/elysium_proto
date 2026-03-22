4000b9cccb09f81cdeabeb42d54d3687975a10b5
DraftedDev <70287588+DraftedDev@users.noreply.github.com>
2026-03-22 13:25:43 +0100

    Replace serde with surrealdb serialization

--- elysium_rust/build.rs
+++ elysium_rust/build.rs
@@ -1,6 +1,20 @@
 use std::path::PathBuf;
 use walkdir::WalkDir;
 
+const IMPORT: &str = "#[allow(unused)] use surrealdb_types::SurrealValue;";
+const IMPORT_AT: &[&str] = &[
+    "chat.v1.SendMessageRequest",
+    "chat.v1.Content.content",
+    "chat.v1.UpdateMessageResponse.result",
+    "chat.v1.SendMessageResponse.result",
+    "common.v1.Error",
+    "user.v1.UserRole",
+    "user.v1.GetUserResponse.result",
+    "resource.v1.ResourceId",
+    "resource.v1.DownloadResponse.result",
+    "resource.v1.UploadRequest.payload",
+];
+
 fn main() {
     let protos = WalkDir::new("../proto")
         .into_iter()
@@ -15,12 +29,21 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
-    tonic_prost_build::configure()
+    let mut builder = tonic_prost_build::configure()
         .build_server(true)
         .build_client(false)
-        .emit_rerun_if_changed(true)
-        .type_attribute(".", "#[derive(serde::Serialize)]")
-        .type_attribute(".", "#[derive(serde::Deserialize)]")
+        .emit_rerun_if_changed(true);
+
+    // SurrealDB Derive Import Workaround:
+    //
+    // This is a workaround for the issue of `derive(surrealdb_types_derive::SurrealValue)` needing an import of `SurrealValue`,
+    // but there is no way of adding one line of code to each prost-generated module, so we add the import to one type of each module.
+    for import in IMPORT_AT.iter() {
+        builder = builder.type_attribute(import, IMPORT);
+    }
+
+    builder
+        .type_attribute(".", "#[derive(surrealdb_types_derive::SurrealValue)]")
         .compile_protos(&protos, &[PathBuf::from("../proto")])
         .expect("Failed to compile protocol buffers");
 }
